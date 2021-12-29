0f9be871d25ac11e1c9f51b1a20a8ef397b72e68
Ayikanying <Ayikanying@users.noreply.github.com>
2021-12-30 01:42:17 +0300

    added the form for commenting so that users are able to post comments

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from core.views import comments, detail_page, homepage
+from core.views import post_comments, detail_page, homepage
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("detail/<str:pk>/", detail_page, name="detail"),
-    path('detail/<str:pk>/comments/', comments, name="comments")
+    path('detail/<str:pk>/comments/', post_comments, name="comments"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import House
+from .form import CommentForm
+from django.views import generic
 
 def homepage(request):
     estates = House.objects.all()
@@ -8,12 +10,28 @@ def homepage(request):
 
 def detail_page(request, pk):
     estates = House.objects.get(id=pk)
-    context = {'estates': estates}
+    form = CommentForm
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.instance.house = House.objects.get(id=pk)
+            form.save()
+            context = {
+                'estates': estates,
+                "form": form,
+            }
+            return render(request, 'core/detail_page.html', context)
+    context = {
+        'estates': estates,
+        "form": form,
+    }
     return render(request, 'core/detail_page.html', context)
 
-def comments(request, pk):
+def post_comments(request, pk):
     estates = House.objects.get(id=pk)
     context={
         'estates': estates
     }
     return render(request, 'core/comments.html', context)
+
