f8097c79e0f571fe67f03343057c0681935d5eda
Ayikanying <Ayikanying@users.noreply.github.com>
2022-01-12 19:27:20 +0300

    debugging code

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 from .forms import NewUserForm
 from django.contrib import messages
@@ -9,7 +9,10 @@ def signup(request):
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, "Registration successful." )
             return redirect("homepage")
