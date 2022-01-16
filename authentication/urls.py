5893fdaede9895963151f8f7cd030f522a9c4be6
Ayikanying <Ayikanying@users.noreply.github.com>
2022-01-16 19:03:37 +0300

    adding user authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from authentication.views import signup
 
 urlpatterns = [
     path("", signup, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="authentication/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="authentication/logout.html"), name="logout"),
     ]
--- authentication/views.py
+++ authentication/views.py
@@ -10,12 +10,7 @@ def signup(request):
         form = NewUserForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "Registration successful." )
-            return redirect("homepage")
+            return redirect("login")
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     context = {"form": form}
