ae7ba9acc70c5cff8b06ce177efeb26f6220fd36
Ayikanying <Ayikanying@users.noreply.github.com>
2022-01-23 22:09:54 +0300

    settings

--- estates/settings.py
+++ estates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(45g#w$!5hcepv3da1s*10!@rj^l+sf9^@w(49%1lqn@er37(7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djanogo-realestate.herokuapp.com', '127.0.0.1']
 
